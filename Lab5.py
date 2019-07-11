61056eb08f0badb6de9277c5631dc53f6fa19911
unknown <chunzen1998@gmail.com>
2019-07-11 11:09:37 -0400

    Demodiulacion

--- Lab5.py
+++ Lab5.py
@@ -14,12 +14,12 @@ import matplotlib.pyplot as plt
 # bt = bit time
 # fs = sample frequency
 
-def ASK(bits, a1, a0, bt, fs):
+def OOK(bits, a1, bt, fs):
 
     tSignal = []
     signal = []
 
-
+    a0 = 0
     a = 0
     for i in bits:
 
@@ -37,18 +37,56 @@ def ASK(bits, a1, a0, bt, fs):
 
 
 
-    plt.figure("Portadora ASK 1")
+    plt.figure("Portadora OOK 1")
     plt.plot(tSignal, signal, "c")
     plt.xlabel("Tiempo(s)")
     plt.ylabel("Amplitud [dB]")
-    plt.title("Portadora ASK")
+    plt.title("Portadora OOK")
     plt.show()
+
 
     return signal, tSignal
 
+
+def demodOOK(signal, a1, bt, fs):
+
+    t = np.linspace(0, bt, num=fs * bt * 100)
+    a0 = 0
+
+    bits = []
+
+    carry1 = a1 * np.cos(2 * np.pi * fs * t)
+    carry0 = a0 * np.cos(2 * np.pi * fs * t)
+    yAux = []
+
+    i = 0
+    while i < len(signal):
+        if i % (fs * bt * 100) == 0:
+            #if(np.correlate(yAux,carry1)[0] == 0):
+            if (signal[i] != 0):
+                bits.append(1)
+            #elif(np.correlate(yAux, carry0)[0] == 0):
+            elif (signal[i] == 0):
+                bits.append(0)
+            yAux = []
+        yAux.append(signal[i])
+        i += 1
+
+    return bits
+
 # ---------- BLOQUE PRINCIPAL -------------
 
-bits = [0, 1, 0, 1, 1]
+bits = [1, 0, 1, 0, 1, 1, 1]
 
-signalASK,tASK = ASK(bits, 2, 1, 1, 2)
 
+
+
+
+signalOOK,tOOK = OOK(bits, 2, 1, 10)
+
+
+bitsDemod = demodOOK(signalOOK, 2, 1, 10)
+
+print(bitsDemod)
+
+#print(np.correlate(np.array([123,1,1]), np.array([0,1,0])))
